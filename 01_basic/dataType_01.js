8138ed559b16f3e42c191566d0f0982cbb7fd126
Mritunjay Kumar <144582396+MritunjayKumar07@users.noreply.github.com>
2023-09-29 08:24:18 +0000

    dataType

---
+++ 01_basic/dataType_01.js
@@ -0,0 +1,30 @@
+let numberType = 123   
+let strintType = "Hello "  
+let booleanType = false
+
+//PRIMITIVE DATATYPE:-
+
+//number => 3124
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value //ex: val = "", val = null
+//undefined => ex: val
+//symbol => unique
+
+console.table([typeof null,typeof undefined]);
+// return :- 
+// ┌─────────┬─────────────┐
+// │ (index) │   Values    │   
+// ├─────────┼─────────────┤
+// │    0    │  'object'   │ //null is object it's language problom or null define object by language
+// │    1    │ 'undefined' │
+// └─────────┴─────────────┘
+
+//NON-PRIMITIVE:-
+
+//object => ex:square = {};,circle = {a: 20, b: 30};,mycar = new Car();,obj = new Object()
+//Array =>  ex:a = new Array();,b = new Array(10);,d = new Array(1, 2, 3, "Hello");
+
+//CHEAK TYPE OF DATA:-
+
