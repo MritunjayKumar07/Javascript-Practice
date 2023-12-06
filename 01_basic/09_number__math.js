109f612b850cfee3cdc1662239f0f3e5e8bcf80a
Mritunjay Kumar <144582396+MritunjayKumar07@users.noreply.github.com>
2023-12-06 04:11:15 +0000

    commit

--- 01_basic/09_number__math.js
+++ 01_basic/09_number__math.js
@@ -15,4 +15,12 @@ console.log(balance.toFixed(2)) //100.00
 // valueOf:ƒ valueOf()
 
 
-//----------------------MATHS--------------------------
+//----------------------MATHS Library--------------------------
+console.log(Math)//return all function in math in google console
+//abs() function change - to + not + to -
+console.log(Math.abs(-4)) //return :4  
+console.log(Math.round(4.3)) //return :4  
+console.log(Math.ceil(4.3)) //return :5 (top value select 4 sa thoda bhe jayada hoga to top vlalue choose) 
+console.log(Math.floor(4.9)) //return :4 (lower value select 5 sa thoda bhe kam hoga to lower vlalue choose) 
+console.log(Math.min(4, 8, 1, 0, 9)) //return 0 (lower value select ) 
+console.log(Math.random() * 10) //return 5.989898qr98 (any random number under 10 degit) 
