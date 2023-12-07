444fc169882c5961d8ae068b9cf4345704c5ed02
Mritunjay Kumar <144582396+MritunjayKumar07@users.noreply.github.com>
2023-12-07 09:01:32 +0530

    DateTIme

--- 01_basic/09_number__math.js
+++ 01_basic/09_number__math.js
@@ -1,6 +1,6 @@
 const score = 400
 console.log(score) //400
-const balance = new Number(100)
+const balance = new Number(100);
 console.log(balance) //[Numbe:100]
 console.log(balance.toString().length) //3
 console.log(balance.toFixed(2)) //100.00
@@ -13,7 +13,6 @@ console.log(balance.toFixed(2)) //100.00
 // toPrecision:ƒ toPrecision()
 // toString:ƒ toString()
 // valueOf:ƒ valueOf()
-
 
 //----------------------MATHS Library--------------------------
 console.log(Math)//return all function in math in google console
@@ -72,3 +71,52 @@ console.log(Math.floor(Math.random() * (max - min +1))+min)
 // tanh:ƒ tanh()
 // trunc:ƒ trunc()
 // Symbol(Symbol.toStringTag):"Math"
+
+//----------------------DATES Library--------------------------
+// month start from 0
+let myDate = new Date(); 
+console.log(typeof myDate) // return: object
+console.log(myDate) // return: 2023-12-07T02:44:58.064Z
+console.log(myDate.toString()) // return: Thu Dec 07 2023 08:14:58 GMT+0530 (India Standard Time)
+console.log(myDate.toDateString()) // return:Thu Dec 07 2023
+console.log(myDate.toISOString()) // return:2023-12-07T02:47:51.214Z
+console.log(myDate.toJSON()) // return:2023-12-07T02:47:51.214Z
+console.log(myDate.toLocaleString()) // return:12/7/2023, 8:17:51 AM
+console.log(myDate.toLocaleDateString()) // return:12/7/2023
+console.log(myDate.toTimeString()) // return:08:17:51 GMT+0530 (India Standard Time)
+
+console.log(myDate.getFullYear()) // return:2023
+console.log(myDate.getMonth()) // return:11
+console.log(myDate.getDay()) // return:4
+console.log(myDate.getHours()) // return:8
+console.log(myDate.getMinutes()) // return:21
+console.log(myDate.getDate()) // return:7
+console.log(myDate.getTime()) // return:1701917490092
+
+let myDefineDate = new Date(2022, 0, 23);
+console.log(myDefineDate.toDateString()) // return: Sun Jan 23 2022 
+console.log(myDefineDate.toLocaleDateString()) // return: 1/23/2022
+
+let myDefineDateTime = new Date(2022, 0, 23, 7, 8);
+console.log(myDefineDateTime.toLocaleString()) // return 1/23/2022, 7:08:00 AM
+
+
+let myCustomeDefineDateTime = new Date("2022-01-23");
+console.log(myCustomeDefineDateTime.toLocaleString()) // return: 1/23/2022, 5:30:00 AM
+
+let myCustomeDefineDateTime2 = new Date("01-23-2023");//(MM-DD-YYYY)
+console.log(myCustomeDefineDateTime2.toLocaleString()) // return: 1/23/2023, 5:30:00 AM
+
+//use for who fast give answer:-
+let myTimeStamp =Date.now()// or you use new.Date
+console.log(myTimeStamp)// return in milisecond  :-1701919138629
+console.log(Math.floor(myTimeStamp/1000))// convert to second return:1701919496
+console.log(myCustomeDefineDateTime2.getTime())// return in milisecond  :-1674412200000
+console.log(myTimeStamp > myCustomeDefineDateTime2.getTime())//compair
+
+
+console.log(new Date().toLocaleString('default',{
+    weekday:"long",
+    year:"2-digit",
+    //etc value use (ctrl + space) to know all calue.
+})) //23 Thursday 
