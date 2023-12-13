bbf959bf4394c9ca04757b4536d34e624261dd7d
Mritunjay Kumar <144582396+MritunjayKumar07@users.noreply.github.com>
2023-12-13 07:18:48 +0530

    Object's

--- 02_basic/04_Objects.js
+++ 02_basic/04_Objects.js
@@ -8,6 +8,12 @@ TinderObject.name = 'Mritunjay'
 TinderObject.isLoggedIn = false
 
 console.log(TinderObject); // return :- { id: '123abc', name: 'Mritunjay', isLoggedIn: false }
+console.log(Object.keys(TinderObject)) //return:- [ 'id', 'name', 'isLoggedIn' ]
+console.log(Object.values(TinderObject)) //return:- [ '123abc', 'Mritunjay', false ]
+console.log(Object.entries(TinderObject)) //return:- [[ 'id', '123abc' ],[ 'name', 'Mritunjay' ],[ 'isLoggedIn', false ]]
+console.log(TinderObject.hasOwnProperty('name')) //hasOwnProperty() this property cheak is key avelable or not. return:- true
+console.log(TinderObject.hasOwnProperty('namemk')) //return:- false
+
 
 const regularUser = {
     email:"mk@mk.com",
@@ -36,4 +42,49 @@ console.log(obj4); // return :- { '1': 'a', '2': 'b', '3': 'c', '4': 'b', '5': '
 console.log(obj5); // return :- { '1': 'a', '2': 'b', '3': 'c', '4': 'b', '5': 'e' }
 console.log(obj6); // return :- { '1': 'a', '2': 'b', '3': 'c', '4': 'b', '5': 'e' }
 
-//-------------------------------------------Time :- 4:15:00-------------------------------------------------
+
+//Come Data From Data Base (Handle):-
+const DataBaseUser = [
+    {
+        id:1,
+        email:'mk@gmail.com',
+    },
+    {
+        id:2,
+        email:'mk1@gmail.com',
+    },
+    {
+        id:3,
+        email:'mk2@gmail.com',
+    },
+    {
+        id:4,
+        email:'mk3@gmail.com',
+    },
+    {
+        id:5,
+        email:'mk4@gmail.com',
+    },
+]
+console.log(DataBaseUser[1].email)
+
+
+
+
+
+
+//All Property in Objects:-
+// constructor:ƒ Object()
+// hasOwnProperty:ƒ hasOwnProperty()
+// isPrototypeOf:ƒ isPrototypeOf()
+// propertyIsEnumerable:ƒ propertyIsEnumerable()
+// toLocaleString:ƒ toLocaleString()
+// toString:ƒ toString()
+// valueOf:ƒ valueOf()
+// __defineGetter__:ƒ __defineGetter__()
+// __defineSetter__:ƒ __defineSetter__()
+// __lookupGetter__:ƒ __lookupGetter__()
+// __lookupSetter__:ƒ __lookupSetter__()
+// __proto__:(...)
+// get __proto__:ƒ __proto__()
+// set __proto__:ƒ __proto__()
