44954fc454a6fb2f403ab48349899cea0ad61e30
Mritunjay Kumar <144582396+MritunjayKumar07@users.noreply.github.com>
2023-12-13 07:50:19 +0530

    commit

--- 02_basic/04_Objects.js
+++ 02_basic/04_Objects.js
@@ -16,11 +16,11 @@ console.log(TinderObject.hasOwnProperty('namemk')) //return:- false
 
 
 const regularUser = {
-    email:"mk@mk.com",
-    fullName:{
-        userfullname:{
-            firstName:"Mritunjay",
-            lastName:"Kumar"
+    email: "mk@mk.com",
+    fullName: {
+        userfullname: {
+            firstName: "Mritunjay",
+            lastName: "Kumar"
         }
     }
 }
@@ -29,15 +29,15 @@ console.log(regularUser.fullName)// return :-{ userfullname: { firstName: 'Mritu
 console.log(regularUser.fullName.userfullname)// return :-{ firstName: 'Mritunjay', lastName: 'Kumar' } 
 console.log(regularUser.fullName.userfullname.firstName)// return :-Mritunjay
 
-const Obj1 = {1:"a", 2:"b"}
-const Obj2 = {3:"c", 4:"b", 5:"e"}
+const Obj1 = { 1: "a", 2: "b" }
+const Obj2 = { 3: "c", 4: "b", 5: "e" }
 
-const obj3 = {Obj1, Obj2};
+const obj3 = { Obj1, Obj2 };
 console.log(obj3); // return :- {Obj1: { '1': 'a', '2': 'b' },Obj2: { '3': 'c', '4': 'b', '5': 'e' }}
 
 const obj4 = Object.assign(Obj1, Obj2)
 const obj5 = Object.assign({}, Obj1, Obj2) // Correct Way
-const obj6 = {...Obj1, ...Obj2} // Correct Way
+const obj6 = { ...Obj1, ...Obj2 } // Correct Way
 console.log(obj4); // return :- { '1': 'a', '2': 'b', '3': 'c', '4': 'b', '5': 'e' }
 console.log(obj5); // return :- { '1': 'a', '2': 'b', '3': 'c', '4': 'b', '5': 'e' }
 console.log(obj6); // return :- { '1': 'a', '2': 'b', '3': 'c', '4': 'b', '5': 'e' }
@@ -46,27 +46,54 @@ console.log(obj6); // return :- { '1': 'a', '2': 'b', '3': 'c', '4': 'b', '5': '
 //Come Data From Data Base (Handle):-
 const DataBaseUser = [
     {
-        id:1,
-        email:'mk@gmail.com',
+        id: 1,
+        email: 'mk@gmail.com',
     },
     {
-        id:2,
-        email:'mk1@gmail.com',
+        id: 2,
+        email: 'mk1@gmail.com',
     },
     {
-        id:3,
-        email:'mk2@gmail.com',
+        id: 3,
+        email: 'mk2@gmail.com',
     },
     {
-        id:4,
-        email:'mk3@gmail.com',
+        id: 4,
+        email: 'mk3@gmail.com',
     },
     {
-        id:5,
-        email:'mk4@gmail.com',
+        id: 5,
+        email: 'mk4@gmail.com',
     },
 ]
 console.log(DataBaseUser[1].email)
+
+
+//Object Destructuring:-
+let person = {
+    name: "John Doe",
+    age: 30,
+    cityName: "New York"
+}
+
+console.log(person.cityName) // 1th Way
+const {cityName: city, age, name} = person // 2th Way (Correct Way) (cityName: city, Change the variable city = cityName)
+console.log([city, age, name])
+
+
+//Api:-
+/*Api comes in json Like this
+{
+    "name":"mritunjay",
+    "coursename":"Js in Hindi",
+    "price":"free"
+}
+We hold in one variable in fruntend*/
+
+//Call Api from JSON:-
+
+
+
 
 
 
