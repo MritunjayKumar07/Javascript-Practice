edfdbb435e99eb4f6be30423fce64df1d297def3
[username] <[user>
2024-01-13 22:34:47 +0530

    commit

--- test.js
+++ test.js
@@ -1,3 +1,7 @@
 console.log(typeof NaN) // number
 console.log(typeof undefined) // undefine
 console.log([5+"5"], typeof (5+"5")) //55 string
+
+arr = [9, 'mritunjay', true, 452522,  1, "mk", false  ]
+console.log(arr.indexOf('mritunjay'))
+console.log(arr)
