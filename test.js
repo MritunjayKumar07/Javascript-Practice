c349ff626a2b4e511d54c050fe04cb99e13bc6ed
[username] <[user>
2024-01-18 09:26:54 +0530

    commit

--- test.js
+++ test.js
@@ -8,3 +8,10 @@ console.log(["5"+5+5], typeof ("5"+5+5)) //555 string
 arr = [9, 'mritunjay', true, 452522,  1, "mk", false  ]
 console.log(arr.indexOf('mritunjay'))
 console.log(arr)
+
+console.log('------------------------------------------------------------------------------')
+console.log(arr.unshift(500))
+console.log(arr)
+console.log(arr.slice(1, 3))
+console.log(arr.splice(1, 3))
+console.log(arr)
